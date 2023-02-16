6049d934fe5bd6bde83fa7180e65baca1cbcea16
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2023-02-16 11:26:57 +0500

    singly linked list/swap nodes

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -80,6 +80,37 @@ class SinglyLinkedList:
             return
         prev.next = curr.next
 
+    def swap_nodes(self, key1, key2):
+        if key1 == key2:
+            return
+
+        prev1 = None
+        curr1 = self._head
+        while curr1 and curr1.data != key1:
+            prev1 = curr1
+            curr1 = curr1.next
+
+        prev2 = None
+        curr2 = self._head
+        while curr2 and curr2.data != key2:
+            prev2 = curr2
+            curr2 = curr2.next
+
+        if not curr1 or not curr2:
+            return
+
+        if prev1:
+            prev1.next = curr2
+        else:
+            self._head = curr2
+
+        if prev2:
+            prev2.next = curr1
+        else:
+            self._head = curr1
+
+        curr1.next, curr2.next = curr2.next, curr1.next
+
 
 if __name__ == '__main__':
     llist = SinglyLinkedList()
@@ -90,8 +121,10 @@ if __name__ == '__main__':
     llist.prepend(1)
     llist.append(new_node=node)
     llist.insert_after_node(node, 4)
-    llist.delete_by_value(3)
-    llist.delete_by_position(1)
+    llist.append(5)
+    # llist.delete_by_value(3)
+    # llist.delete_by_position(1)
 
-    print(len(llist))
-    print(*llist)
+    print(llist)
+    llist.swap_nodes(2, 3)
+    print(llist)
